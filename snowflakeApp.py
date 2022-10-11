491d3ecbf14950b22a8830c0d8d2f9e11a3b0330
JoonaKhr <Joonakhr@gmail.com>
2022-10-11 13:28:40 +0300

    Max of five objects on screen at a time and code comments, additional functions

--- snowflakeApp.py
+++ snowflakeApp.py
@@ -9,11 +9,11 @@ from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.config import Config
 
-
+#Set the inital window size and the minimum size for computer versions of the game
 Window.size = (500, 600)
 Window.minimum_width = 500
 Window.minimum_height = 600
-
+#Set a nice gray background so it doesn't burn the eyes
 Window.clearcolor = (0.3, 0.3, 0.3, 1)
 
 class SettingsScreen(Screen):
@@ -26,28 +26,48 @@ class LeaderboardsScreen(Screen):
 class OperationSnowflake(Screen):
     score = 0
     scoreText = StringProperty(f"Score: {score}")
-    
+    objectsOnScreen = []
+
+    def on_touch_down(self, touch):
+        return super().on_touch_down(touch)
+
+    #Supposedly adds points when it gets implemented on to the game also updates the score text on screen
     def addPoint(self):
         self.score += 1
         self.scoreText = f"Score: {self.score}"
     
+    #Spawn a game object in a random position and check if the game object list equals five in that case delete the earliest one
     def spawnInRandPos(self):
-        self.ids.playArea.add_widget(Image(source="resources/imgs/a3.png", pos_hint={"center_x":uniform(.05, .90),"center_y":uniform(.05, .90)}, color=(1,1,1,1)), 0, self.ids.playArea.canvas.after)
-
-    def openSettings(self):
-        pass
+        playObject = Image(source="resources/imgs/a3.png", color=(1,1,1,1))
+        playObject.size = ("32dp", "32dp")
+        playObject.pos_hint={"center_x":uniform(.05, .90),"center_y":uniform(.05, .90)}
+        self.ids.playArea.add_widget(playObject, 0, self.ids.playArea.canvas.after)
+        if len(self.objectsOnScreen) == 5:
+            self.ids.playArea.remove_widget(self.objectsOnScreen[0])
+            self.objectsOnScreen.pop(0)
+            self.objectsOnScreen.append(playObject)
+        else:
+            self.objectsOnScreen.append(playObject)
     
+    #Init the timer event to use for the StartGameToggle 
     def startClock(self):
         self.timerEvent = Clock.schedule_interval(self.timerTick, 1)
 
-    def stopClock(self):
+    #Reset the game state on the original values
+    def resetGameState(self):
+        self.ids.startToggle.state = "normal"
+        self.ids.startToggle.text = "Start"
+        self.ids.ldrbrds_btn.disabled = False
+        self.ids.settings_btn.disabled = False
+        self.ids.timer.value = 0
+        self.ids.playArea.clear_widgets()
         self.timerEvent.cancel()
 
+    # Start the game and the clock also disable the two extra buttons so no accidents happen
     def startGameToggle(self, widget):
         if widget.state == "normal":
             widget.text = "Start"
-            self.ids.timer.value = 0
-            self.stopClock()
+            self.resetGameState()
         else:
             widget.text = "Stop"
             self.ids.timer.value = 100
@@ -56,13 +76,13 @@ class OperationSnowflake(Screen):
             self.ids.settings_btn.disabled = True
             self.startClock()
 
+    #Tick tock the time goes also spawns one flake on every tick
     def timerTick(self, dt):
         self.ids.timer.value -= 1
         self.spawnInRandPos()
         if self.ids.timer.value == 0:
-            self.stopClock()
-            self.startToggle.state = "normal"
-            self.startToggle.text = "Start"
+            self.resetGameState()
+            
 
 
 class SnowflakeApp(App):
